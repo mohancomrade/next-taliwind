3e1584a78e6d2b990eff0491111d419c01b4711e
Your Name <you@example.com>
2023-06-01 14:04:52 +0530

    next-tailwind

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,15 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='text-center font-sans'>
-      {/* Next Home Page */}
+    <div className='text-center font'>
+      Next Home Page
 
 
-      {/* <div class="columns-3 ..."> */}
-  <img src="https://www.pixelstalk.net/wp-content/uploads/images6/Itachi-HD-Wallpaper-Free-download.jpg" />
-  {/* <img class="w-full" src="https://i0.wp.com/otakusmart.com/wp-content/uploads/2022/03/Itachis-abilities.jpg" /> */}
+      <div class="columns-3 ...">
+  <img className="w-full" src="https://www.pixelstalk.net/wp-content/uploads/images6/Itachi-HD-Wallpaper-Free-download.jpg" />
+  <img class="w-full" src="https://i0.wp.com/otakusmart.com/wp-content/uploads/2022/03/Itachis-abilities.jpg" />
   
 </div>
-    // </div>
+    </div>
   )
 }
